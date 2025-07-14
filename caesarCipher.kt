9cb0133fa1675b976d639ff0389b3ad9d9aca3de
Kaustubh Suryakant Deshpande <216578302+kaustubhsuryakantdeshpande@users.noreply.github.com>
2025-07-14 08:34:57 +0000

    caesar cipher algo done

---
+++ caesarCipher.kt
@@ -0,0 +1,26 @@
+
+fun main() {
+    println("Enter input to encrypt the text")
+    val enteredString = "abcDefGhI"
+    val result = caesarCipherEncrypt(enteredString)
+    println("The encrypted text is - $result")
+}
+
+fun caesarCipherEncrypt(stringToEncrypt: String): String {
+    var encryptedString = ""
+    for (char in stringToEncrypt) {
+        val encryptedChar = when (char) {
+            in 'a'..'z' -> {
+                val shiftedSmall = ((char - 'a' + 3) % 26) + 'a'.toInt()
+                shiftedSmall.toChar()
+            }
+            in 'A'..'Z' -> {
+                val shiftedLarge = ((char - 'A' + 3) % 26) + 'A'.toInt()
+                shiftedLarge.toChar()
+            }
+            else -> char
+        }
+        encryptedString += encryptedChar
+    }
+    return encryptedString
+}
